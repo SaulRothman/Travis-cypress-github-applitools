e82ae094547218eea0c9d622a8201e8457eecfa7
appliSaul <saul.rothman@applitools.com>
2020-11-03 14:35:46 +0200

    Second Commit

--- cypress/integration/examples/cicd.spec.js
+++ cypress/integration/examples/cicd.spec.js
@@ -9,8 +9,8 @@ describe("AppTest", () => {
         cy.visit('https://demo.applitools.com');
         // Call Open on eyes to initialize a test session
         cy.eyesOpen({
-            appName: 'Demo Apps',
-            testName: 'Ultrafast grid demo',
+            appName: 'Demo App',
+            testName: 'UltraFast grid demo',
         })
         // check the login page with fluent api, see more info here
         // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
